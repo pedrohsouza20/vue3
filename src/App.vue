b22ed6a2f55fddd967ace0498e9498690bf01e29
Pedro <contato.pedromoc@gmail.com>
2023-03-12 00:18:39 -0300

    Entendendo Life Cycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <PrimeiroComponent />
+  <LifeCycle />
 </template>
 
 <script>
 import PrimeiroComponent from "./components/PrimeiroComponent.vue";
+import LifeCycle from "./components/LifeCycle.vue";
 
 export default {
   name: "App",
   components: {
     PrimeiroComponent,
+    LifeCycle,
   },
 };
 </script>
