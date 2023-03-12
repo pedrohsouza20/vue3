14c8b8279dba6cf342bda2504e966ddff4961b34
Pedro <contato.pedromoc@gmail.com>
2023-03-12 02:09:19 -0300

    Hierarquia de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <PrimeiroComponent />
   <LifeCycle />
+  <Pessoa />
 </template>
 
 <script>
 import PrimeiroComponent from "./components/PrimeiroComponent.vue";
 import LifeCycle from "./components/LifeCycle.vue";
+import Pessoa from "./components/Pessoa.vue";
 
 export default {
   name: "App",
   components: {
     PrimeiroComponent,
     LifeCycle,
+    Pessoa,
   },
 };
 </script>
